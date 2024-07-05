31bf4e77e66eeebc49a5b5da865f6263933469ff
Eduardo Menezes <eduardo.santosm1202@gmail.com>
2024-07-05 12:25:03 -0300

    Funcionalidades de Bipagem adicionada

--- app/static/js/bipagem.js
+++ app/static/js/bipagem.js
@@ -1,3 +1,20 @@
+let lastProductNumber = 1;
+
+let productCount = 0;
+
+function dataTable() {
+    const now = new Date();
+    let day = now.getDate().toString().padStart(2, '0');
+    let month = (now.getMonth() + 1).toString().padStart(2, '0');
+    let year = now.getFullYear();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
+
+    return `${day}/${month}/${year} - ${hours}:${minutes}:${seconds}`;
+}
+dataTable();
+
 function updateClock() {
 
     const now = new Date();
@@ -18,32 +35,131 @@ function updateClock() {
 }
 updateClock();
 
-document.addEventListener("DOMContentLoaded", function() {
+function getFormattedDate() {
+
+    const today = new Date();
+
+    let day = today.getDate().toString().padStart(2, '0');
+    let month = (today.getMonth() + 1).toString().padStart(2, '0');
+    let year = today.getFullYear();
+
+    const dataString = `${day}/${month}/${year}`;
+
+    document.getElementById('realdata').textContent = "Data: " + dataString;
+}
+getFormattedDate();
+
+
+function getNextProductNumber() {
+    let newNumber = lastProductNumber;
+    lastProductNumber += 4;
+    return newNumber.toString().padStart(4, '0');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const codigoProdutoInput = document.getElementById('codigoProduto');
+    const chaveProdutoInput = document.getElementById('chaveProduto');
+    const tbody = document.getElementById('table-body');
+    const productCountSpan = document.getElementById('product-count');
+    let isDataBeingProcessed = false;
+
+    function updateProductCount() {
+        productCount++;
+        productCountSpan.textContent = productCount;
+    }
+
+    codigoProdutoInput.addEventListener('input', function () {
+        chaveProdutoInput.disabled = !codigoProdutoInput.value.trim();
+    });
+
+    chaveProdutoInput.addEventListener('input', function () {
+        if (codigoProdutoInput.value.trim() && chaveProdutoInput.value.trim() && !isDataBeingProcessed) {
+            isDataBeingProcessed = true;
+            setTimeout(function () {
+                addDataToTable(tbody, codigoProdutoInput.value, chaveProdutoInput.value);
+                updateProductCount();
+                codigoProdutoInput.value = '';
+                chaveProdutoInput.value = '';
+                chaveProdutoInput.disabled = true;
+                isDataBeingProcessed = false;
+                codigoProdutoInput.focus();
+            }, 1000);
+        }
+    });
+
+    function addDataToTable(tbody, codigo, chave) {
+        const newRow = tbody.insertRow(-1);
+        const newCell1 = newRow.insertCell(0);
+        const newCell2 = newRow.insertCell(1);
+        const newCell3 = newRow.insertCell(2);
+        const newCell4 = newRow.insertCell(3);
+        const newCell5 = newRow.insertCell(4);
+        const newCell6 = newRow.insertCell(5);
+
+        const checkBox = document.createElement('input');
+        checkBox.type = 'checkbox';
+        newCell1.appendChild(checkBox);
+
+        newCell2.textContent = getNextProductNumber();
+        newCell3.textContent = codigo;
+        newCell4.textContent = chave;
+        newCell5.textContent = dataTable();
+        newCell6.textContent = 'Ações';
+
+        const avisoIniciado = document.querySelector('.aviso-iniciado');
+        avisoIniciado.style.display = 'none';
+    }
+});
+
+function checkAndToggleAviso(tbody, avisoIniciado) {
+    if (tbody.children.length > 0) {
+        avisoIniciado.style.display = 'none';
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     const btnIniciar = document.querySelector('.button-iniciar');
     const btnCancelar = document.querySelector('.button-cancelar');
     const btnFinalizar = document.querySelector('.button-finalizar');
 
-    btnIniciar.addEventListener('click', function() {
+    btnIniciar.addEventListener('click', function () {
         btnIniciar.style.display = 'none';
 
         btnCancelar.style.display = 'inline-block';
         btnFinalizar.style.display = 'inline-block';
     });
 
-    btnCancelar.addEventListener('click', function() {
+    btnCancelar.addEventListener('click', function () {
         btnIniciar.style.display = 'inline-block';
         btnCancelar.style.display = 'none';
         btnFinalizar.style.display = 'none';
     });
 
-    btnFinalizar.addEventListener('click', function() {
+    btnFinalizar.addEventListener('click', function () {
         btnIniciar.style.display = 'inline-block';
         btnCancelar.style.display = 'none';
         btnFinalizar.style.display = 'none';
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
+function clearTbody(tbody) {
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+}
+
+function resetProductCount() {
+    productCount = 0;
+
+    const productCountSpan = document.getElementById('product-count');
+
+    if (productCountSpan) {
+        productCountSpan.textContent = productCount;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     const btnIniciar = document.querySelector('.button-iniciar');
     const btnCancelar = document.querySelector('.button-cancelar');
     const btnFinalizar = document.querySelector('.button-finalizar');
@@ -58,27 +174,56 @@ document.addEventListener("DOMContentLoaded", function() {
     const tbody = document.querySelector('tbody');
     const avisoInicio = document.querySelector('.aviso-inicio');
     const avisoIniciado = document.querySelector('.aviso-iniciado');
+    const codigoProdutoInput = document.getElementById('codigoProduto');
+    const chaveProdutoInput = document.getElementById('chaveProduto');
 
-    btnIniciar.onclick = function() {
+    function toggleInputs(enabled) {
+        const codigoProdutoInput = document.getElementById('codigoProduto');
+        const chaveProdutoInput = document.getElementById('chaveProduto');
+
+        codigoProdutoInput.disabled = !enabled;
+        chaveProdutoInput.disabled = true;
+        if (!enabled) {
+            codigoProdutoInput.value = '';
+            chaveProdutoInput.value = '';
+        }
+    }
+
+    codigoProdutoInput.addEventListener('input', function () {
+        if (codigoProdutoInput.value.trim() !== '') {
+            setTimeout(function () {
+                chaveProdutoInput.focus();
+            }, 1000);
+        }
+    });
+
+    btnIniciar.onclick = function () {
+        resetProductCount();
         btnIniciar.style.display = 'none';
         btnCancelar.style.display = 'inline';
         btnFinalizar.style.display = 'inline';
         avisoInicio.style.display = 'none';
         avisoIniciado.style.display = 'flex';
-
-        //tbody.style.display = 'table-row-group';
+        toggleInputs(true);
+        tbody.style.display = 'table-row-group';
+        codigoProdutoInput.focus();
     };
 
-    btnCancelar.onclick = function() {
+    btnCancelar.onclick = function () {
         avisoInicio.style.display = 'none';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'none';
         btnCancelar.style.display = 'inline';
         btnFinalizar.style.display = 'inline';
         confirmationCancelModal.style.display = 'flex';
+        codigoProdutoInput.value = '';
+        chaveProdutoInput.value = '';
     };
 
-    btnCancelarPopup.onclick = function() {
+    btnCancelarPopup.onclick = function () {
+        toggleInputs(true);
+        codigoProdutoInput.value = '';
+        chaveProdutoInput.value = '';
         avisoInicio.style.display = 'none';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'none';
@@ -87,7 +232,10 @@ document.addEventListener("DOMContentLoaded", function() {
         confirmationModal.style.display = 'none';
     };
 
-    btnCancelarOperação.onclick = function() {
+    btnCancelarOperação.onclick = function () {
+        toggleInputs(true);
+        codigoProdutoInput.value = '';
+        chaveProdutoInput.value = '';
         avisoInicio.style.display = 'none';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'none';
@@ -96,17 +244,22 @@ document.addEventListener("DOMContentLoaded", function() {
         confirmationCancelModal.style.display = 'none';
     }
 
-    btnConfirmarExcluirOperação.onclick = function() {
+    btnConfirmarExcluirOperação.onclick = function () {
+        toggleInputs(false);
+        codigoProdutoInput.value = '';
+        chaveProdutoInput.value = '';
         avisoInicio.style.display = 'flex';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'inline';
         btnCancelar.style.display = 'none';
         btnFinalizar.style.display = 'none';
         confirmationCancelModal.style.display = 'none';
+        clearTbody(tbody);
+        resetProductCount();
     }
 
-    btnFinalizar.onclick = function() {
-        avisoInicio.style.display = 'flex';
+    btnFinalizar.onclick = function () {
+        avisoInicio.style.display = 'none';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'none';
         btnCancelar.style.display = 'inline';
@@ -114,26 +267,31 @@ document.addEventListener("DOMContentLoaded", function() {
         confirmationModal.style.display = 'flex';
     };
 
-    document.querySelector('.button-confirm').onclick = function() {
+    document.querySelector('.button-confirm').onclick = function () {
         confirmationModal.style.display = 'none';
         loadingPopup.style.display = 'flex';
-        setTimeout(function() {
+        setTimeout(function () {
             loadingPopup.style.display = 'none';
             finalizadoPopup.style.display = 'flex';
+            clearTbody(tbody);
+            toggleInputs(false);
+            avisoInicio.style.display = 'flex';
+            resetProductCount();
         }, 3000);
     };
 
-    btnConcluir.onclick = function() {
+    btnConcluir.onclick = function () {
         avisoInicio.style.display = 'flex';
         avisoIniciado.style.display = 'none';
         btnIniciar.style.display = 'inline';
         btnCancelar.style.display = 'none';
         btnFinalizar.style.display = 'none';
         finalizadoPopup.style.display = 'none';
+        clearTbody(tbody);
     };
 
     document.querySelectorAll('.close-button-confirm').forEach(button => {
-        button.onclick = function() {
+        button.onclick = function () {
             avisoInicio.style.display = 'none';
             avisoIniciado.style.display = 'none';
             confirmationCancelModal.style.display = 'none';
@@ -144,7 +302,7 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 
     document.querySelectorAll('.close-button-concluido').forEach(button => {
-        button.onclick = function() {
+        button.onclick = function () {
             avisoInicio.style.display = 'none';
             avisoIniciado.style.display = 'none';
             btnIniciar.style.display = 'inline';
@@ -155,8 +313,8 @@ document.addEventListener("DOMContentLoaded", function() {
             loadingPopup.style.display = 'none';
         };
     });
-});
 
-function closeFinalizadoPopup() {
-    document.getElementById('popup-finalizado-container').style.display = 'none';
-}
+    codigoProdutoInput.addEventListener('input', function () {
+        chaveProdutoInput.disabled = !codigoProdutoInput.value.trim();
+    });
+});
