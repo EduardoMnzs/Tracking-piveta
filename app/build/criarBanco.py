9b2432d121b98ee09fbe89147b60e2dd3cf77bb9
Eduardo Menezes <eduardo.santosm1202@gmail.com>
2024-10-24 17:26:24 -0300

    Tabela de Ticket/status

--- app/build/criarBanco.py
+++ app/build/criarBanco.py
@@ -10,7 +10,7 @@ con = psycopg2.connect(
 
 cur = con.cursor()
 
-bip = '''CREATE TABLE produtos (
+bip = '''CREATE TABLE IF NOT EXISTS produtos (
     id SERIAL PRIMARY KEY,
     codigo_interno VARCHAR(50) NOT NULL,
     codigo VARCHAR(50) NOT NULL,
@@ -34,6 +34,25 @@ respostas = '''CREATE TABLE IF NOT EXISTS respostas_ml (
     resposta VARCHAR(255) NOT NULL
 );'''
 cur.execute(respostas)
+con.commit()
+
+status = '''CREATE TABLE IF NOT EXISTS status (
+    id SERIAL PRIMARY KEY NOT NULL,
+    status VARCHAR(25) NOT NULL
+);'''
+cur.execute(status)
+con.commit()
+
+ticket = '''CREATE TABLE IF NOT EXISTS tickets (
+    id SERIAL PRIMARY KEY NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    status INT NOT NULL,
+    anexos TEXT NOT NULL,
+    data TIMESTAMP NOT NULL,
+    CONSTRAINT fk_status FOREIGN KEY (status) REFERENCES status(id) ON DELETE CASCADE
+);'''
+cur.execute(ticket)
 con.commit()
 
 con.close()
